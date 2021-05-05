dff450347615ea421d349213917701714fb2466a
ryo-endo <to.endo.dev@gmail.com>
2021-05-05 12:13:01 +0900

    [Refactor]121. Best Time to Buy and Sell Stock

--- 121_best-time-to-buy-and-sell-stock/Solution.py
+++ 121_best-time-to-buy-and-sell-stock/Solution.py
@@ -3,16 +3,11 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        min_price = None
-        max_price = None
+        min_price = prices[0]
         profit = 0
 
         for price in prices:
-            if min_price is None or min_price > price:
-                min_price = price
-                max_price = None
-            elif max_price is None or max_price < price:
-                max_price = price
-                profit = max(profit, max_price - min_price)
+            min_price = min(min_price, price)
+            profit = max(profit, price - min_price)
 
         return profit
