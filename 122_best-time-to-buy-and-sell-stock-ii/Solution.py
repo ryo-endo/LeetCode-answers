6a5bf64ab4850ba64c282957aa32e3fc461e29a0
ryo-endo <to.endo.dev@gmail.com>
2021-05-10 23:19:26 +0900

    [Refactor]122. Best Time to Buy and Sell Stock II - Approach 3: Simple One Pass

--- 122_best-time-to-buy-and-sell-stock-ii/Solution.py
+++ 122_best-time-to-buy-and-sell-stock-ii/Solution.py
@@ -5,14 +5,10 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         total_profit = 0
         last_price = prices[0]
-        profit = 0
+
         for price in prices[1:]:
-            if price < last_price:
-                last_price = price
-                continue
-            profit += price - last_price
+            if last_price < price:
+                total_profit += price - last_price
             last_price = price
-            total_profit += profit
-            profit = 0
 
         return total_profit
